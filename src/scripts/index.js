ffd8a83f2b9fbe4a4821ef225f41bece3b5c5a93
akavaliova <kovaleva89alena@gmail.com>
2023-02-24 17:25:12 +0100

    Made  a modal window

---
+++ src/scripts/index.js
@@ -0,0 +1,21 @@
+"use strict"
+// Moduele window functions
+
+
+
+// функция не рабочая
+// function dropdownFunction() {
+//     document.getElementById("modal-window__dropdownWrap").classList.toggle("show");
+//   }
+//   // Закрыть раскрывающийся список, если пользователь щелкнет за его пределами.
+//   window.onclick = function(event) {
+//     if (!event.target.matches('modal-window__dropdownBtn')) {
+//       let dropdowns = document.getElementsByClassName("modal-window__dropdownContent");
+//       for (let i = 0; i < dropdowns.length; i++) {
+//         let openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
