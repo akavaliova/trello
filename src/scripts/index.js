6e07ae2c781be164a22bdc81855bbb8352e75f0f
akavaliova <kovaleva89alena@gmail.com>
2023-02-27 18:32:52 +0100

    create initialfunctions for adding todo

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,63 +1,79 @@
 "use strict"
 
-// Open and close modal window
+// Open and close modal window:
 const overlayWrap = document.getElementById("overlay");
 const modalEditBtn = document.getElementById("trello__todo-active--editBtn");
 const addTodoBtn = document.getElementById("trello__todo-active--addTodoBtn");
 const modalCancelBtn = document.getElementById("modal-window__cancelBtn");
 const modalConfirmBtn = document.getElementById("modal-window__confirmBtn");
+const titleInput = document.getElementById("modal-window__title");
+const descriptionInput = document.getElementById("modal-window__text");
 
+function addTodo(title, description) {
+    // Create TODO object and put it to the local storage
+    // {id = '', title = '', state = 'TODO, In Progress, DONE' etc}
+    alert(title);
+    alert(description)
+    alert(generateId());
+    alert(getCurentDate());
+    alert("Not done");
+}
 
+function generateId() {
+    return 1;
+}
 
-modalEditBtn.addEventListener("click", function () {
-    overlayWrap.classList.add("active");
-})
+function getCurentDate() {
+    return new Date();
+}
+
+function clearInputFields() {
+    titleInput.value = '';
+    descriptionInput.value = '';
+}
 
 addTodoBtn.addEventListener("click", function () {
     overlayWrap.classList.add("active");
-})
+});
 
 modalCancelBtn.addEventListener("click", function () {
     overlayWrap.classList.remove("active");
-})
-//временно:
+    clearInputFields();
+});
+// временно:
 modalConfirmBtn.addEventListener("click", function () {
-    const descriptionText = document.getElementById("modal-window__text").value; 
-    if(!!descriptionText){
-        console.log(descriptionText);  // сохраняю в консоль введенный в поле input текст
+    const title = titleInput.value;
+    const description = descriptionInput.value;
+
+    if (!!description && !!title) {
         overlayWrap.classList.remove("active"); // закрываю модальное окно
-        // не работает, не очищает поле input!!!!!!!!!!!!!!!!!!!!!!!!!!
-        descriptionText.value = "";
+        clearInputFields();
+        addTodo(title, description);
+    } else {
+        alert("Please, input the title and the description");
     }
-})
+});
 
 
-
-//warning modal window
+// warning modal window:
 const transferBtn = document.getElementById("trello__todo-active--transferBtn");
 const warningOverlay = document.getElementById("overlayWarning");
 const warningCancelBtn = document.getElementById("modal-warning__cancelBtn");
 const warningConfirmBtn = document.getElementById("modal-warning__confirmBtn");
 
 
-transferBtn.addEventListener("click", function () {
-    warningOverlay.classList.add("show");
-})
-
-
-
 warningCancelBtn.addEventListener("click", function () {
     warningOverlay.classList.remove("show");
 })
 
-//временно
+//временно:
 
 warningConfirmBtn.addEventListener("click", function () {
     console.log("I agree to add 6 more cases");
     warningOverlay.classList.remove("show");
 })
 
-//Clock
+// Clock
 function updateClock() {
     let now = new Date(); // текущая дата и время
     let hours = now.getHours(); // часы
