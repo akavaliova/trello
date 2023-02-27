be1fe37a5d65ca7b1fdd36a2e5452edd9b34eac1
akavaliova <kovaleva89alena@gmail.com>
2023-02-27 21:09:02 +0100

    add todo to local storage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,8 @@
 "use strict"
 
 // Open and close modal window:
+const statusTODO = "TODO";
+const statusDONE = "DONE";
 const overlayWrap = document.getElementById("overlay");
 const modalEditBtn = document.getElementById("trello__todo-active--editBtn");
 const addTodoBtn = document.getElementById("trello__todo-active--addTodoBtn");
@@ -9,22 +11,66 @@ const modalConfirmBtn = document.getElementById("modal-window__confirmBtn");
 const titleInput = document.getElementById("modal-window__title");
 const descriptionInput = document.getElementById("modal-window__text");
 
+
+
+function init() {
+    const ulWrap = document.getElementsByClassName("trello__todo-active trello__todo-active--wrap");
+    const activeCase = createElementWithID("li", "trello__todo-active--case");
+    const activeUpperSection = createElementWithClassName("div", "trello__todo-active trello__todo-active--upperSection");
+    const activeTitle = createElementWithClassName("p", "trello__todo-active trello__todo-active--title");
+    const activeEditBtn = createElementWithID("button", "trello__todo-active--editBtn");
+    const activeDelBtn = createElementWithClassName("button", "trello__todo-active trello__todo-active--delBtn");
+
+    const activeMidSection = createElementWithClassName("div", "trello__todo-active trello__todo-active--midSection");
+    const activeDescription = createElementWithClassName("p", "trello__todo-active trello__todo-active--description");
+    const activeTransferBtn = createElementWithID("button", "trello__todo-active--transferBtn");
+
+    const activeDownSection = createElementWithClassName("div", "trello__todo-active trello__todo-active--downSection");
+    const activeUser = createElementWithClassName("p", "trello__todo-active trello__todo-active--user");
+    const activeTime = createElementWithClassName("p", "trello__todo-active trello__todo-active--time");
+  
+    ulWrap.append(activeCase);
+    activeCase.append(activeUpperSection);
+    activeUpperSection.append(activeTitle);
+    activeTitle.append(activeEditBtn);
+    activeEditBtn.append(activeDelBtn);
+    activeDelBtn.append(activeMidSection);
+    activeMidSection.append(activeDescription);
+    activeDescription.append(activeTransferBtn);
+    activeTransferBtn.append(activeDownSection);
+    activeDownSection.append(activeUser);
+    activeUser.append(activeTime);
+  }
+
+  
+
+
 function addTodo(title, description) {
-    // Create TODO object and put it to the local storage
-    // {id = '', title = '', state = 'TODO, In Progress, DONE' etc}
-    alert(title);
-    alert(description)
-    alert(generateId());
-    alert(getCurentDate());
-    alert("Not done");
+    const todos = getTodos();
+  
+    const todo = {
+      title: title,
+      description: description,
+      date: getCurentDate(),
+      id: generateId(),
+      status: statusTODO,
+    };
+
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));  
+}
+
+function getTodos() {
+    const todos = localStorage.getItem("todos");
+    return !!todos ? JSON.parse(todos) : [];
 }
 
 function generateId() {
-    return 1;
+    return Math.floor(Math.random() * 1000);
 }
 
 function getCurentDate() {
-    return new Date();
+    return new Date().toLocaleString();
 }
 
 function clearInputFields() {
@@ -84,3 +130,6 @@ function updateClock() {
   updateClock(); // вызываем функцию updateClock() в первый раз, чтобы отобразить текущее время
   // обновляем время каждую минуту
   setInterval(updateClock, 60000);
+
+  
+  init();
